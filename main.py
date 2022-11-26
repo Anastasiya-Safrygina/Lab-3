cbb8f7c86f2fc257ba068e04c416bb394d8b21bd
Андрей Боев <a.boev.it@gmail.com>
2022-11-26 14:25:41 +0300

    Fix 1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+lines = []
 with open('input.txt') as f:
     lines = f.readlines()
-    max_string = ''
-    max_value = 0
-    for i in range(len(lines)):
-        if lines[i].find('>String') != -1:
-            num = lines[i].strip()
-            dna = lines[i + 1]
-            count = dna.count('G') + dna.count('C')
-            percent = count / len(dna) * 100
-            if percent > max_value:
-                max_value = percent
-                max_string = num
-    print(f'Фрагмент max: {max_string}\nДоля G/C: {max_value}')
+max_string = ''
+max_value = 0
+for i in range(len(lines)):
+    if lines[i].find('>String') != -1:
+        num = lines[i].strip()
+        dna = lines[i + 1]
+        count = dna.count('G') + dna.count('C')
+        percent = count / len(dna) * 100
+        if percent > max_value:
+            max_value = percent
+            max_string = num
+print(f'Фрагмент max: {max_string}\nДоля G/C: {max_value}')
